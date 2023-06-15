a319a871f23f270c2d2f43e9cac9b6ecd0e1fbc1
Antonella Laghezza <a.laghezza@reply.it>
2023-06-15 18:11:49 +0200

    Inizializzazione progetto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> App di Antonella</p>
+      </header>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default navbar;
