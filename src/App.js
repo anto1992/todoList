c62ad615ff0d8554a1948a7eedb4fc4bb54a9778
Sara Mosca <s.mosca@reply.it>
2025-08-27 18:25:29 +0200

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p> App di Antonella</p>
+        <p> App di Test test</p>
       </header>
       <footer></footer>
     </div>
